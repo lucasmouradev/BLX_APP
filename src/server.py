8af4c58b028e577ee428243eb852c83269614197
lucas <lucasmouraadev@gmail.com>
2022-06-28 17:28:51 -0300

    Feita criação do banco de dados do app BLX, adicionado rotas, criação de todos os arquivos e pastas necessárias para o projeto, incluido schema, server, database, models, produto.

---
+++ src/server.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI, Depends, status
+from typing import List
+from sqlalchemy.orm import Session
+from src.schema.schema import Produto, ProdutoSimples
+from src.infra.sqlalchemy.config.database import get_db, criar_bd
+from src.infra.sqlalchemy.repositorio.produto import RepositorioProduto
+
+
+criar_bd()
+
+app = FastAPI()
+
+
+
+@app.post('/produtos', status_code= status.HTTP_201_CREATED, response_model=ProdutoSimples)
+def criar_produto(produto: Produto, db: Session = Depends(get_db)):
+    produto_criado = RepositorioProduto(db).criar(produto)
+    return produto_criado
+
+@app.get('/produtos', status_code= status.HTTP_200_OK, response_model=List[ProdutoSimples])
+def listar_produtos(db: Session = Depends(get_db)):
+    produtos = RepositorioProduto(db).listar()
+    return produtos
