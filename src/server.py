40f85b349f3849a36496ea4e04d65e720843420e
lucas <lucasmouraadev@gmail.com>
2022-07-01 03:21:04 -0300

    Adicionado a rota pedido no server.py e criado as rotas no rotas_pedidos

--- src/server.py
+++ src/server.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from src.routers import rotas_produtos, rotas_usuarios
+from src.routers import rotas_produtos, rotas_usuarios, rotas_pedidos
 
 
 app = FastAPI()
@@ -22,3 +22,8 @@ app.include_router(rotas_produtos.router)
 # ROTAS USUARIOS
 
 app.include_router(rotas_usuarios.router)
+
+
+# ROTAS PEDIDOS 
+
+app.include_router(rotas_pedidos.router)
