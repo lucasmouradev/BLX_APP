043f597ea09790b828b999700c74cb4801d55bb2
lucas <lucasmouraadev@gmail.com>
2022-07-01 01:29:04 -0300

    Refatoração do arquivo server.py, tornando mais objetivo e simples de fazer a manuntenção, criado a pasta de rotas para futuras rotas

--- src/server.py
+++ src/server.py
@@ -1,54 +1,24 @@
-from fastapi import FastAPI, Depends, status
-from typing import List
-from sqlalchemy.orm import Session
-from src.schema.schema import Produto, ProdutoSimples, Usuario
-from src.infra.sqlalchemy.config.database import get_db, criar_bd
-from src.infra.sqlalchemy.repositorio.repositorio_produto import RepositorioProduto
-from src.infra.sqlalchemy.repositorio.repositorio_usuario import RepositorioUsuario
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from src.routers import rotas_produtos, rotas_usuarios
 
-
-#criar_bd()
 
 app = FastAPI()
 
-# PRODUTOS
+# CORS
 
-@app.post('/produtos', status_code= status.HTTP_201_CREATED, response_model=ProdutoSimples)
-def criar_produto(produto: Produto, db: Session = Depends(get_db)):
-    produto_criado = RepositorioProduto(db).criar(produto)
-    return produto_criado
+origins = ['http://localhost:3000']
 
-@app.get('/produtos', response_model=List[Produto])
-def listar_produtos(db: Session = Depends(get_db)):
-    produtos = RepositorioProduto(db).listar()
-    return produtos
+app.add_middleware(CORSMiddleware,
+                    allow_origins=origins,
+                    allow_credentials=True,
+                    allow_methods=["*"],
+                    allow_headers=["*"] )
 
+# ROTAS PRODUTOS
 
+app.include_router(rotas_produtos.router)
 
-@app.put('/produtos/{id}', response_model=ProdutoSimples)
-def atualizar_produto(id: int, produto: Produto, session: Session = Depends(get_db)):
-    RepositorioProduto(session).editar(id, produto)
-    produto.id = id
-    return produto 
+# ROTAS USUARIOS
 
-
-
-@app.delete('/produtos/{id}')
-def remover_produto(id: int, session: Session = Depends(get_db)):
-    RepositorioProduto(session).remover(id)
-    return 
-
-
-# USUARIOS
-
-@app.post('/signup', status_code=status.HTTP_201_CREATED, response_model=Usuario)
-def signup(usuario:Usuario, session: Session = Depends(get_db)):
-    usuario_criado = RepositorioUsuario(session).criar(usuario)
-    return usuario_criado
-
-
-
-@app.get('/usuarios', response_model=List[Usuario])
-def listar_usuario(session: Session = Depends(get_db)):
-    usuarios = RepositorioUsuario(session).listar()
-    return usuarios
+app.include_router(rotas_usuarios.router)
